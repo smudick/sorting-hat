ef9b91c749fbe35300778700ef671719652cb777
Sam Mudick <smudick@gmail.com>
2020-07-29 21:14:34 -0500

    added delete functionality

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 "use strict";
+const houses = ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"];
 
 const initialButtonClick = () => {
   document.querySelector("#sortButton").addEventListener("click", buildForm);
@@ -36,26 +37,48 @@ const nameButtonClick = () => {
   document.querySelector("#nameButton").addEventListener("click", getName);
   document.querySelector("#nameButton").addEventListener("click", buildCard);
 };
+
+const expelButtonClick = () => {
+  document.querySelector("#studentCard").addEventListener("click", deleteItems);
+};
+
 let studentNames = [];
 
 const getName = () => {
   const name = document.getElementById("inlineFormInput").value;
-  return studentNames.push(name);
+  studentNames.push({ name: name, house: houses[getHouse()] });
+};
+
+const getHouse = () => {
+return Math.floor(Math.random() * 4);
 };
 
 const buildCard = () => {
   let domString = "";
-  const houses = ['Gryffindor', 'Slytherin', 'Hufflepuff', 'Ravenclaw']
-  
+
   for (let i = 0; i < studentNames.length; i++) {
-    let housePicker = Math.floor(Math.random() * 4)
     domString += `<div class="card" style="width: 18rem;">`;
     domString += `<div class="card-body">`;
-    domString += `<h5 class="card-title">${studentNames[i]}</h5>`;
-    domString += `<h6 class="card-subtitle mb-2 text-muted">${houses[housePicker]}</h6>`;
+    domString += `<h5 class="card-title">${studentNames[i].name}</h5>`;
+    domString += `<h6 class="card-subtitle mb-2 text-muted">${studentNames[i].house}</h6>`;
+    domString += `<button type="button" class="btn btn-danger" id=${i}>Expel</button>`;
     domString += `</div></div>`;
   }
   printToDom("studentCard", domString);
+  expelButtonClick();
+};
+
+const deleteItems = (e) => {
+  console.log("Delete Me!", e.target.id);
+
+  const ctype = e.target.type;
+  const target = e.target.id;
+
+  if (ctype === "button") {
+    studentNames.splice(target, 1);
+
+    buildCard();
+  }
 };
 
 const init = () => {
